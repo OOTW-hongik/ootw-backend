4de3d582ea580b58e8e356379692660011c6c630
morenow <jjang9877@gmail.com>
2023-05-14 01:56:42 +0900

    Feat: Implement Swagger, Use RDS

--- src/main/java/OOTWhongik/OOTW/controller/HomeController.java
+++ src/main/java/OOTWhongik/OOTW/controller/HomeController.java
@@ -1,11 +1,17 @@
 package OOTWhongik.OOTW.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "swagger", description = "swagger test")
 @RestController
 public class HomeController {
 
+    @Operation(summary = "home", description = "home test")
+    @ApiResponse(responseCode = "200", description="ok")
     @GetMapping("/home")
     public String home() {
         return "home";
