7d1393f2c62c0a79db6cffe510a007952960bcae
morenow <jjang9877@gmail.com>
2023-06-19 02:23:23 +0900

    Feat: 홈 화면에 착장 리스트 3개 출력 기능 추가

--- src/main/java/OOTWhongik/OOTW/dto/HomeResponse.java
+++ src/main/java/OOTWhongik/OOTW/dto/HomeResponse.java
@@ -4,14 +4,18 @@ import OOTWhongik.OOTW.domain.Member;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 public class HomeResponse {
     private final String name;
     private final String location;
+    private final List<OutfitResponse> outfitResponseList;
 
     @Builder
-    public HomeResponse(Member member) {
+    public HomeResponse(Member member, List<OutfitResponse> outfitResponseList) {
         this.name = member.getName();
         this.location = member.getLocation();
+        this.outfitResponseList = outfitResponseList;
     }
 }
