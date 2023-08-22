0602a8864a8f21acee1c6213303c662501923611
morenow <jjang9877@gmail.com>
2023-08-22 23:00:23 +0900

    Fix: 일부 HttpMethod에 대한 CORS 오류 해결

--- src/main/java/OOTWhongik/OOTW/OotwApplication.java
+++ src/main/java/OOTWhongik/OOTW/OotwApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -20,7 +21,17 @@ public class OotwApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000", "http://ootw.s3-website.ap-northeast-2.amazonaws.com/");
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:3000",
+										"http://ootw.s3-website.ap-northeast-2.amazonaws.com/")
+						.allowedMethods(
+								HttpMethod.GET.name(),
+								HttpMethod.POST.name(),
+								HttpMethod.PUT.name(),
+								HttpMethod.DELETE.name(),
+								HttpMethod.PATCH.name(),
+								HttpMethod.HEAD.name()
+						);
 			}
 		};
 	}
