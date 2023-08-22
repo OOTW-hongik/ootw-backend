d49b4de1045c0db643e9b163be335bf85c04cf14
morenow <jjang9877@gmail.com>
2023-08-22 23:35:42 +0900

    Fix: 모든 HttpMethod에 대해 CORS 설정

--- src/main/java/OOTWhongik/OOTW/OotwApplication.java
+++ src/main/java/OOTWhongik/OOTW/OotwApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -24,14 +23,7 @@ public class OotwApplication {
 				registry.addMapping("/**")
 						.allowedOrigins("http://localhost:3000",
 										"http://ootw.s3-website.ap-northeast-2.amazonaws.com/")
-						.allowedMethods(
-								HttpMethod.GET.name(),
-								HttpMethod.POST.name(),
-								HttpMethod.PUT.name(),
-								HttpMethod.DELETE.name(),
-								HttpMethod.PATCH.name(),
-								HttpMethod.HEAD.name()
-						);
+						.allowedMethods("*");
 			}
 		};
 	}
