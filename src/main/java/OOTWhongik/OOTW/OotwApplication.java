fc00d0bdfc956a3955d84eca73623387e1545877
morenow <jjang9877@gmail.com>
2023-08-28 17:25:21 +0900

    Chore: CORS 허용 주소 변경

--- src/main/java/OOTWhongik/OOTW/OotwApplication.java
+++ src/main/java/OOTWhongik/OOTW/OotwApplication.java
@@ -21,8 +21,8 @@ public class OotwApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:3000",
-										"http://ootw.s3-website.ap-northeast-2.amazonaws.com/")
+						.allowedOrigins("http://localhost:3000/",
+										"http://ootw.store.s3-website.ap-northeast-2.amazonaws.com/")
 						.allowedMethods("*");
 			}
 		};
