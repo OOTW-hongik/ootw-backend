f2f10af83adb9575cf9db09c9289be14f616499c
morenow <jjang9877@gmail.com>
2023-09-10 21:22:19 +0900

    Feat: health_check API 추가

---
+++ src/main/java/OOTWhongik/OOTW/global/controller/GlobalController.java
@@ -0,0 +1,20 @@
+package OOTWhongik.OOTW.global.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "Global", description = "Global")
+@RequiredArgsConstructor
+@RestController
+public class GlobalController {
+
+    @Operation(summary = "health_check", description = "health_check")
+    @GetMapping("/health_check")
+    public ResponseEntity<?> healthCheck() {
+        return ResponseEntity.ok().build();
+    }
+
+}
