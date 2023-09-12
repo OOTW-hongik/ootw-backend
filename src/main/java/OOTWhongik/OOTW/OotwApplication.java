75862f02240e1f901ff82d0fabba76268ca0d611
morenow <jjang9877@gmail.com>
2023-09-12 15:35:41 +0900

    Fix: HTTPS 변경으로 Swagger 설정 (Server URL) 변경

--- src/main/java/OOTWhongik/OOTW/OotwApplication.java
+++ src/main/java/OOTWhongik/OOTW/OotwApplication.java
@@ -1,5 +1,7 @@
 package OOTWhongik.OOTW;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -7,6 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @EnableJpaAuditing
 @SpringBootApplication
 public class OotwApplication {
