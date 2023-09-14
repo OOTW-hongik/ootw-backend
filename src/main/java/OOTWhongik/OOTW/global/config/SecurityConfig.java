18e630560ea37857599766e64b7ad288bedfe72b
morenow <jjang9877@gmail.com>
2023-09-14 18:13:05 +0900

    Remove: Spring Security 및 이전 OAuth 구현 관련 파일 삭제, 로그인 버튼 제거

--- src/main/java/OOTWhongik/OOTW/global/config/SecurityConfig.java
+++ src/main/java/OOTWhongik/OOTW/global/config/SecurityConfig.java
@@ -1,35 +1,35 @@
-package OOTWhongik.OOTW.global.config;
-
-import OOTWhongik.OOTW.global.config.auth.CustomOAuth2UserService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@RequiredArgsConstructor
-@EnableWebSecurity
-public class SecurityConfig { //WebSecurityConfigurerAdapter was deprecated
-
-    private final CustomOAuth2UserService customOAuth2UserService;
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.authorizeRequests()
-                .anyRequest().permitAll()
-//			  .antMatchers("/**").authenticated() // 인가된 사용자만 접근 가능하도록 설정
-//			  .antMatchers("게시물등").hasRole(Role.USER.name()) // 특정 ROLE을 가진 사용자만 접근 가능하도록 설정
-                .and()
-                .logout()
-                .logoutSuccessUrl("/")
-                .and()
-                .oauth2Login()
-                .defaultSuccessUrl("/")
-                .userInfoEndpoint()
-                .userService(customOAuth2UserService);
-        return http.build();
-    }
-}
+//package OOTWhongik.OOTW.global.config;
+//
+//import OOTWhongik.OOTW.global.config.auth.CustomOAuth2UserService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@RequiredArgsConstructor
+//@EnableWebSecurity
+//public class SecurityConfig { //WebSecurityConfigurerAdapter was deprecated
+//
+//    private final CustomOAuth2UserService customOAuth2UserService;
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.csrf().disable();
+//        http.authorizeRequests()
+//                .anyRequest().permitAll()
+////			  .antMatchers("/**").authenticated() // 인가된 사용자만 접근 가능하도록 설정
+////			  .antMatchers("게시물등").hasRole(Role.USER.name()) // 특정 ROLE을 가진 사용자만 접근 가능하도록 설정
+//                .and()
+//                .logout()
+//                .logoutSuccessUrl("/")
+//                .and()
+//                .oauth2Login()
+//                .defaultSuccessUrl("/")
+//                .userInfoEndpoint()
+//                .userService(customOAuth2UserService);
+//        return http.build();
+//    }
+//}
