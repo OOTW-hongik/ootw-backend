5249c485ec249898023dd193ced5fff64c151113
morenow <jjang9877@gmail.com>
2023-09-14 18:23:28 +0900

    Refactor: CORS 설정 Config 파일 생성

--- src/main/java/OOTWhongik/OOTW/OotwApplication.java
+++ src/main/java/OOTWhongik/OOTW/OotwApplication.java
@@ -4,12 +4,11 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
+@ConfigurationPropertiesScan
 @EnableJpaAuditing
 @SpringBootApplication
 public class OotwApplication {
@@ -18,17 +17,4 @@ public class OotwApplication {
 		SpringApplication.run(OotwApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:3000/",
-										"https://ootw.store/",
-										"https://www.ootw.store/")
-						.allowedMethods("*");
-			}
-		};
-	}
 }
