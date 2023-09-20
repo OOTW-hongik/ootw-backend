dbdded67ed15a7a25333da435d9a5c06aaa5fbf1
morenow <jjang9877@gmail.com>
2023-09-20 16:26:51 +0900

    Fix: 로그인 시 jwt를 JSON 형식으로 응답하도록 수정

--- src/main/java/OOTWhongik/OOTW/global/application/OauthService.java
+++ src/main/java/OOTWhongik/OOTW/global/application/OauthService.java
@@ -6,8 +6,13 @@ import OOTWhongik.OOTW.domain.member.repository.MemberRepository;
 import OOTWhongik.OOTW.global.auth.authcode.AuthCodeRequestUrlProviderComposite;
 import OOTWhongik.OOTW.global.client.OauthMemberClientComposite;
 import OOTWhongik.OOTW.global.config.security.JwtUtils;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -21,10 +26,14 @@ public class OauthService {
         return authCodeRequestUrlProviderComposite.provide(oauthServerType);
     }
 
-    public String login(OauthServerType oauthServerType, String authCode) {
+    public String login(OauthServerType oauthServerType, String authCode) throws JsonProcessingException {
         Member member = oauthMemberClientComposite.fetch(oauthServerType, authCode);
         Member saved = memberRepository.findByOauthId(member.getOauthId())
                 .orElseGet(() -> memberRepository.save(member));
-        return JwtUtils.createToken(saved);
+        String jws = JwtUtils.createToken(saved);
+        Map<String, String> map = new HashMap<>();
+        map.put("jwt", jws);
+        String ret =  new ObjectMapper().writeValueAsString(map);
+        return ret;
     }
 }
--- src/main/java/OOTWhongik/OOTW/global/presentation/OauthController.java
+++ src/main/java/OOTWhongik/OOTW/global/presentation/OauthController.java
@@ -2,6 +2,7 @@ package OOTWhongik.OOTW.global.presentation;
 
 import OOTWhongik.OOTW.domain.member.domain.OauthServerType;
 import OOTWhongik.OOTW.global.application.OauthService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
@@ -30,7 +31,7 @@ public class OauthController {
     ResponseEntity<?> login(
             @PathVariable OauthServerType oauthServerType,
             @RequestParam("code") String code
-    ) {
+    ) throws JsonProcessingException {
         String login = oauthService.login(oauthServerType, code);
         return ResponseEntity.ok(login);
     }
