d955513ff28ae077fa0ba6eeef9db70c987cbe05
morenow <jjang9877@gmail.com>
2023-12-10 04:45:10 +0900

    feat: Advisor에 처리할 예외 추가
    Outfit 혹은 날씨에 관련된 예외도 처리하도록 추가

--- src/main/java/OOTWhongik/OOTW/global/exception/RestControllerAdvisor.java
+++ src/main/java/OOTWhongik/OOTW/global/exception/RestControllerAdvisor.java
@@ -2,6 +2,9 @@ package OOTWhongik.OOTW.global.exception;
 
 import OOTWhongik.OOTW.domain.clothes.exception.*;
 import OOTWhongik.OOTW.domain.member.exception.LocationNotFoundException;
+import OOTWhongik.OOTW.domain.outfit.exception.InvalidDateException;
+import OOTWhongik.OOTW.domain.outfit.exception.OutfitNotFoundException;
+import OOTWhongik.OOTW.domain.outfit.exception.UnauthorizedOutfitAccessException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.tomcat.util.http.fileupload.impl.SizeLimitExceededException;
@@ -17,7 +20,7 @@ import java.util.Map;
 public class RestControllerAdvisor {
 
     @ExceptionHandler(UnauthorizedClothesAccessException.class)
-    @ResponseStatus(HttpStatus.FORBIDDEN)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public String handleUnauthorizedClothesAccessException() throws JsonProcessingException {
         return responseJsonMessage("옷의 소유주가 아닙니다.");
     }
@@ -56,6 +59,30 @@ public class RestControllerAdvisor {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String handleLocationNotFoundException() throws JsonProcessingException {
         return responseJsonMessage("올바르지 않은 지역명을 입력했습니다.");
+    }
+
+    @ExceptionHandler(InvalidDateException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleInvalidDateException() throws JsonProcessingException {
+        return responseJsonMessage("잘못된 형식의 날짜를 입력했습니다.");
+    }
+
+    @ExceptionHandler(OutfitNotFoundException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleOutfitNotFoundException() throws JsonProcessingException {
+        return responseJsonMessage("착장을 찾을 수 없습니다.");
+    }
+
+    @ExceptionHandler(UnauthorizedOutfitAccessException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public String handleUnauthorizedOutfitAccessException() throws JsonProcessingException {
+        return responseJsonMessage("착장의 소유주가 아닙니다.");
+    }
+
+    @ExceptionHandler(CrawlingException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleCrawlingException() throws JsonProcessingException {
+        return responseJsonMessage("날씨 정보를 가져오는데 실패했습니다.");
     }
 
     private String responseJsonMessage(String message) throws JsonProcessingException {
